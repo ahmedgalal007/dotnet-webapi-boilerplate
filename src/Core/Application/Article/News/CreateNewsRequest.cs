063cd1c5fb9f1af7d7150ffbe8a255ba2a4def50
Ahmed Galal <ahmedgalal007@hotmail.com>
2024-01-06 04:05:40 +0200

    create news

--- src/Core/Application/Article/News/CreateNewsRequest.cs
+++ src/Core/Application/Article/News/CreateNewsRequest.cs
@@ -12,23 +12,24 @@ public class CreateNewsRequest : IRequest<Guid>
     public string? CultureCode { get; set; } = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
     public string Title { get; set; } = default!;
     public string Slug { get; set; } = default!;
-    public string? Description { get; set; }
-    public string? SubTitle { get; set; }
-    public string? SEOTitle { get; set; }
-    public string? SocialTitle { get; set; }
-    public string? Body { get; set; }
-    public FileUploadRequest? MainImage { get; set; }
+    public string? Description { get; set; } = default!;
+    public string? SubTitle { get; set; } = default!;
+    public string? SEOTitle { get; set; } = default!;
+    public string? SocialTitle { get; set; } = default!;
+    public string? Body { get; set; } = default!;
+
+    public FileUploadRequest? MainImage { get; set; } = default!;
 }
 
-public class CreateNewsRequestValidator : CustomValidator<CreateNewsRequest>
-{
-    public CreateNewsRequestValidator(IReadRepository<Domain.Article.News> repository, IStringLocalizer<CreateNewsRequestValidator> T) =>
-        RuleFor(p => p.Title)
-            .NotEmpty()
-            .MaximumLength(75)
-            .MustAsync(async (title, ct) => await repository.FirstOrDefaultAsync(new NewsByTitleSpec(title), ct) is null)
-                .WithMessage((_, name) => T["News {0} already Exists.", name]);
-}
+//public class CreateNewsRequestValidator : CustomValidator<CreateNewsRequest>
+//{
+//    public CreateNewsRequestValidator(IReadRepository<Domain.Article.News> repository, IStringLocalizer<CreateNewsRequestValidator> T) =>
+//        RuleFor(p => p.Title)
+//            .NotEmpty()
+//            .MaximumLength(75)
+//            .MustAsync(async (title, ct) => await repository.FirstOrDefaultAsync(new NewsByTitleSpec(title), ct) is null)
+//                .WithMessage((_, name) => T["News {0} already Exists.", name]);
+//}
 
 public class CreateNewsRequestHandler : IRequestHandler<CreateNewsRequest, Guid>
 {
@@ -43,10 +44,11 @@ public class CreateNewsRequestHandler : IRequestHandler<CreateNewsRequest, Guid>
     public async Task<Guid> Handle(CreateNewsRequest request, CancellationToken cancellationToken)
     {
         string lang = request.CultureCode ?? Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
-        string newsImagePath = await _file.UploadAsync<Product>(request.MainImage, FileType.Image, cancellationToken);
+        // string newsImagePath = await _file.UploadAsync<Product>(request.MainImage, FileType.Image, cancellationToken);
         string slug = request.Slug ?? _seoUtilitiesService.Slugify(request.Title);
 
-        var news = new Domain.Article.News(request.Title, slug, request.Description, request.Body, request.SubTitle, request.SEOTitle, request.SocialTitle, lang, newsImagePath);
+        //var news = new Domain.Article.News(request.Title, slug, request.Description, request.Body, request.SubTitle, request.SEOTitle, request.SocialTitle, lang, newsImagePath);
+        var news = new Domain.Article.News(request.Title, slug, request.Description, request.Body, request.SubTitle, request.SEOTitle, request.SocialTitle, lang, "");
         news.DomainEvents.Add(EntityCreatedEvent.WithEntity(news));
         await _repository.AddAsync(news, cancellationToken);
 
---
+++ src/Core/Application/Article/News/EventHandlers/NewsCreatedEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using FSH.WebApi.Application.Catalog.Products.EventHandlers;
+using FSH.WebApi.Domain.Article;
+using FSH.WebApi.Domain.Common.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Application.Article.News.EventHandlers;
+public class NewsCreatedEventHandler : EventNotificationHandler<EntityCreatedEvent<Domain.Article.News>>
+{
+    private readonly ILogger<NewsCreatedEventHandler> _logger;
+    public NewsCreatedEventHandler(ILogger<NewsCreatedEventHandler> logger) => _logger = logger;
+    public override Task Handle(EntityCreatedEvent<Domain.Article.News> @event, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("{event} Triggered", @event.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
